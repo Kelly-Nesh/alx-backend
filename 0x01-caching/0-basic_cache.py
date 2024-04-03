91d2d19eeb6dda3345c688946835c7bf2d8f4651
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-04-04 00:26:32 +0300

    0-update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
-""" Base Caching module
-"""
+'''Basic caching module.
+'''
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BaseCache:
-      - caching system without limit
-    """
-
-    def __init__(self):
-        """init method. Calls superclass init"""
-        super().__init__()
+    '''Represents an object that allows storing and
+    retrievinf items from a dictionary
+    '''
 
     def put(self, key, item):
-        """Assign to the dictionary self.cache_data
-        the item value for the key key."""
-        if key is not None or item is not None:
-            self.cache_data[key] = item
+        '''Insert an item in to the cache
+        '''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key."""
-        return self.cache_data.get(key)
+        '''Return an item by key.
+        '''
+        return self.cache_data.get(key, None)
