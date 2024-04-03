8513544df9bb443720323790d440af7169b8eeeb
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-04-04 00:10:35 +0300

    removed-init-method

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,48 +1,13 @@
 #!/usr/bin/env python3
-""" BaseCaching module
+""" Base Caching module
 """
-
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError(
-            "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError(
-            "get must be implemented in your cache class")
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ BaseCache:
       - caching system without limit
     """
-
-    def __init__(self):
-        """init method. Calls superclass init"""
-        super().__init__()
 
     def put(self, key, item):
         """Assign to the dictionary self.cache_data
@@ -52,4 +17,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """return the value in self.cache_data linked to key."""
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
